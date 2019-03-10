018d12b441cdb92a7ba4a6e70ef4cc8acca7a484
Matthew Underwood <matthewsunderwood@gmail.com>
2019-03-10 03:43:58 -0700

    new navbar

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,23 +1,21 @@
 <template>
-  <nav class="navbar navbar-expand-sm navbar-light bg-light" id="navbar">
-    <ul class="navbar-nav">
-      <li class="nav-item" id="home-link">
-        <router-link class="nav-link" to="/">Home</router-link>
-      </li>
-      <li class="nav-item" id="interactivity-link">
-        <router-link class="nav-link" to="interactivity">Interactivity</router-link>
-      </li>
-      <li class="nav-item" id="scroll-link">
-        <router-link class="nav-link" to="scroll">Scrolling</router-link>
-      </li>
-      <li class="nav-item" id="mutate-link">
-        <router-link class="nav-link" to="mutate">Mutation</router-link>
-      </li>
-      <li class="nav-item" id="end-link">
-        <router-link class="nav-link" to="End">End</router-link>
-      </li>
-    </ul>
-  </nav>
+  <div class="navbar" id="navbar">
+    <div class="nav-item" id="home-link">
+      <router-link class="nav-link" to="/">Home</router-link>
+    </div>
+    <div class="nav-item" id="interactivity-link">
+      <router-link class="nav-link" to="interactivity">Interactivity</router-link>
+    </div>
+    <div class="nav-item" id="scroll-link">
+      <router-link class="nav-link" to="scroll">Scrolling</router-link>
+    </div>
+    <div class="nav-item" id="mutate-link">
+      <router-link class="nav-link" to="mutate">Mutation</router-link>
+    </div>
+    <div class="nav-item" id="end-link">
+      <router-link class="nav-link" to="End">End</router-link>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -36,13 +34,22 @@ export default {
 <style scoped>
 
 .navbar {
-  position: fixed !important;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  background: white;
+  position: fixed;
   top: 0;
-  width: 100% !important;
+  width: 100%;
 }
 
 .nav-item {
+  display: flex;
   margin-left: 10px;
   margin-right: 10px;
+}
+
+a {
+  color: #333;
 }
 </style>
