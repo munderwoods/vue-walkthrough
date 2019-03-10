c460ed8e6137c148369e7ea13ebb46f44018768c
Matthew Underwood <matthewsunderwood@gmail.com>
2019-03-10 04:12:51 -0700

    bar fixed for real

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,20 +1,10 @@
 <template>
-  <div class="navbar" id="navbar">
-    <div class="nav-item" id="home-link">
-      <router-link class="nav-link" to="/">Home</router-link>
-    </div>
-    <div class="nav-item" id="interactivity-link">
-      <router-link class="nav-link" to="interactivity">Interactivity</router-link>
-    </div>
-    <div class="nav-item" id="scroll-link">
-      <router-link class="nav-link" to="scroll">Scrolling</router-link>
-    </div>
-    <div class="nav-item" id="mutate-link">
-      <router-link class="nav-link" to="mutate">Mutation</router-link>
-    </div>
-    <div class="nav-item" id="end-link">
-      <router-link class="nav-link" to="End">End</router-link>
-    </div>
+  <div class="bar" id="navbar">
+    <router-link class="link" id="home-link" to="/">Home</router-link>
+    <router-link class="link" id="interactivity-link" to="interactivity">Interactivity</router-link>
+    <router-link class="link" id="scroll-link" to="scroll">Scrolling</router-link>
+    <router-link class="link" id="mutate-link" to="mutate">Mutation</router-link>
+    <router-link class="link" id="end-link" to="End">End</router-link>
   </div>
 </template>
 
@@ -33,21 +23,20 @@ export default {
 
 <style scoped>
 
-.navbar {
+.bar {
   display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  align-items: center;
   background: white;
   position: fixed;
   top: 0;
   width: 100%;
-  height: 50px;
+  height: 60px;
+  align-items: center;
 }
 
-.nav-item {
-  margin-left: 20px;
-  margin-right: 20px;
+.link {
+  margin: 0 20px 0 20px; 
+  display: flex;
+  align-items: center;
 }
 
 a {
